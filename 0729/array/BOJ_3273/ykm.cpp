f8b5820554fcfe466e0cb818c9558671f6bed85a
Yu Kyeongmin(Pheobe) <fibikong@naver.com>
2024-07-30 20:22:27 +0900

    Update ykm.cpp
    재도전 성공

--- 0729/array/BOJ_3273/ykm.cpp
+++ 0729/array/BOJ_3273/ykm.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 
 int main(void){
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::ios::sync_with_stdio(0);
+    const int MX = 2000005;
     int n, x;
     std::cin >> n;
-    int arr[100000]={0};
+    bool arr[MX]={false};
     for(int i = 0; i<n; i++){
-        std::cin >> arr[i];
+        //서다 양의 정수
+        std::cin >> x;
+        arr[x]=true;
     }
     std::cin >> x;
-
-    char check[2000001]={0};
     int cnt = 0;
-    for(int i = 0; i<n; i++){
-        if(check[x-arr[i]]){cnt+=check[x-arr[i]];}
-        check[arr[i]]++;
+    for(int i = 1; i<(x+1)/2; i++){ // x가 3일때 x/2시 동작하지 않았었음
+        if(arr[i] && arr[x-i]){
+            // std::cout << i << ' ';
+            cnt++;
+        }
     }
     std::cout << cnt;
 
